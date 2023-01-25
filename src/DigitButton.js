cfb1a7a66f68543601abab4947bc745b57cf2e9f
chadbrb1 <91875699+chadbrb1@users.noreply.github.com>
2023-01-25 22:52:10 +0100

    added style of the calculator

---
+++ src/DigitButton.js
@@ -0,0 +1,4 @@
+import {ACTIONS} from './App'
+export default function DigitButton({dispatch, digit}){
+    return <button onClick = {()=> dispatch({type : ACTIONS.digit, payload :{digit}})}>{digit}</button>
+}
